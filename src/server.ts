42cbdcb2e3b67d03bb229fdeb5570a7556fc4a24
JohnSavage <dr.dany@yandex.ru>
2021-06-29 18:27:28 +0300

    dev

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => {
+  res.status(200).json({msg: 'Good!'});
+});
+
+app.listen(port, () => {
+  console.log(`Server start on: localhost:${port}`);
+});
