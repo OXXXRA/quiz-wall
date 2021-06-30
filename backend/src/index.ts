93f9eb94bbddf8af99c858e303d2ae906d8ddef4
bikishov <bikishovd@gmail.com>
2021-06-30 10:06:58 +0300

    feat: start server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('test');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Good!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server start on: localhost:${port}`);
+});
